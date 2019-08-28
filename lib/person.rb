11a98a0ab667916768d30f29f9894ec4b5bd637c
Christopher Kennedy <chriswkennedy@icloud.com>
2019-08-28 15:03:45 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  
+  def initialize(person_name)
+    @person_name = person_name
+  end
+  
+  def person_name
+    @person_name
+  end
+  
+  def person_name=(person_name)
+    @person_name = person_name
+  end
+  
+  
+  
+end
